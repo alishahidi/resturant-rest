1f33156a33ab4f54eea4ac1b6cf1f7681dbe2180
Ali Shahidi <alishahidi1376@gmail.com>
2023-08-20 11:03:16 +0330

    init commit

---
+++ src/main/java/com/neshan/resturantrest/ResturantRestApplication.java
@@ -0,0 +1,13 @@
+package com.neshan.resturantrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResturantRestApplication.class, args);
+    }
+
+}
