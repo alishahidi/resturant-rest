d0617bde9d3b9304c92ce25c6e7ad01817ea4ba8
Ali Shahidi <alishahidi1376@gmail.com>
2023-08-20 11:51:44 +0330

    add User model

---
+++ src/main/java/com/neshan/resturantrest/models/User.java
@@ -0,0 +1,16 @@
+package com.neshan.resturantrest.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String id;
+    private String name;
+    private String username;
+}
