db67357bdaa1bbfab3cc9bd87c27dd88937431a1
Ali Shahidi <alishahidi1376@gmail.com>
2023-08-20 18:19:25 +0330

    add quantity to Food model

--- src/main/java/com/neshan/resturantrest/models/Food.java
+++ src/main/java/com/neshan/resturantrest/models/Food.java
@@ -1,18 +1,17 @@
 package com.neshan.resturantrest.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Food {
     private String id;
     private String name;
     private Double price;
     private String restaurantId;
+    private Integer quantity;
 
 }
