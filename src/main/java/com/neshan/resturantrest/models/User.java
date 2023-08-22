6d9309517ceda86ce045c01e4338fa13b7588e5d
Ali Shahidi <alishahidi1376@gmail.com>
2023-08-22 12:45:48 +0330

    implement UserDetails in User model

--- src/main/java/com/neshan/resturantrest/models/User.java
+++ src/main/java/com/neshan/resturantrest/models/User.java
@@ -1,14 +1,51 @@
 package com.neshan.resturantrest.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     private String id;
     private String name;
     private String username;
+    @JsonIgnore
+    private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
