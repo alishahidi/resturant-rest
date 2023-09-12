f6ec768c4b332df37eaca73a08cf7096d6862821
Ali Shahidi <alishahidi1376@gmail.com>
2023-09-12 22:15:00 +0330

    jackson postgis config

---
+++ src/main/java/com/neshan/resturantrest/config/JtsConfiguration.java
@@ -0,0 +1,15 @@
+package com.neshan.resturantrest.config;
+
+import org.n52.jackson.datatype.jts.JtsModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JtsConfiguration {
+
+    @Bean
+    public JtsModule jtsModule() {
+        // This module will provide a Serializer for geometries
+        return new JtsModule();
+    }
+}
